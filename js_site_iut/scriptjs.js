99d2122b4b1eeef04fe903ce68015eacf276d55e
mathis Guerin <mathis.guerin@etu.univ-lyon1.fr>
2021-12-28 16:09:03 +0100

    le js qui tue ca mere sur l'accueil

--- js_site_iut/scriptjs.js
+++ js_site_iut/scriptjs.js
@@ -1,14 +1,48 @@
-$(window).scroll(function() {
+const sep = document.querySelector('.separateur');
+const div = document.querySelector('.carousel');
+let hauteur = div.clientHeight;
 
-    var scrollPos = $(this).scrollTop() /20;
-    console.log(scrollPos);
+const boxone = document.querySelector('.boxinfo-1');
+const boxtwo = document.querySelector('.boxinfo-2');
+const boxthree = document.querySelector('.boxinfo-3');
+const boxfour = document.querySelector('.boxinfo-4');
 
-    $('.right-content').css ({
-        'left' : 0 + scrollPos + 'px'
-    })
+window.addEventListener('scroll', () => {
 
-    $('.left-content').css ({
-        'right' : 0 + scrollPos + 'px'
-    })
+    // console.log(window.scrollY);
 
-})
+    if(window.scrollY > hauteur) {
+        sep.classList.add('scroll');
+    }
+    else {
+        sep.classList.remove('scroll');
+    }
+
+    if(window.scrollY > (hauteur - 300)) {
+        boxone.classList.add('scroll');
+    }
+    else {
+        boxone.classList.remove('scroll');
+    }
+
+    if(window.scrollY > hauteur - 100) {
+        boxtwo.classList.add('scroll');
+    }
+    else {
+        boxtwo.classList.remove('scroll');
+    }
+
+    if(window.scrollY > hauteur + 100) {
+        boxthree.classList.add('scroll');
+    }
+    else {
+        boxthree.classList.remove('scroll');
+    }
+
+    if(window.scrollY > hauteur + 300) {
+        boxfour.classList.add('scroll');
+    }
+    else {
+        boxfour.classList.remove('scroll');
+    }
+});
